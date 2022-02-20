4c9aab6d6a586aba89277a73af1bd60380b1844f
odjhey <odjhey@gmail.com>
2022-02-20 20:44:04 +0800

    refactor(client): move to using a hook

--- pages/client/[me]/[target].tsx
+++ pages/client/[me]/[target].tsx
@@ -1,22 +1,22 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
-const ConnectPage = () => {
-  const router = useRouter();
-  const { me, target } = router.query;
-  const [mePeer, setMePeer] = useState<any>(null);
-  const [conn, setConn] = useState<any>(null);
-  const [messages, setMessages] = useState<string[]>(["defa"]);
-  const [message, setMessage] = useState("");
+const usePeerConn = (myId: string, peers: string | string[]) => {
+  const [meAsPeer, setMeAsPeer] = useState<any>(null);
+  const [conns, setConns] = useState<any[]>([]);
 
   useEffect(() => {
-    if (!(me && target)) {
+    console.log("try to init peer conn with my id");
+    const lpeers = Array.isArray(peers) ? peers : [peers];
+
+    if (!myId && lpeers.length) {
       return;
     }
+
+    console.log("vali pass");
+
     import("peerjs").then((peerjs) => {
-      console.log("me", me);
-      console.log("target", target);
-      const peer = new peerjs.default(me as string, {
+      const peer = new peerjs.default(myId as string, {
         host: "localhost",
         port: 9000,
         path: "/myapp",
@@ -25,27 +25,45 @@ const ConnectPage = () => {
       peer.on("connection", (conn) => {
         conn.on("data", (data) => {
           // Will print 'hi!'
-          setMessages((messages) => [...messages, data]);
           console.log(data);
         });
         conn.on("error", (err) => {
-          setMessages((messages) => [...messages, `ERROR: ${err}`]);
           console.error(err);
         });
       });
-      setMePeer(peer);
 
-      const conn = peer.connect(target as string);
-      conn.on("open", () => {
-        console.log("open");
-        conn.send("hi!");
+      const conns = lpeers.map((p) => {
+        const conn = peer.connect(p);
+        conn.on("open", () => {
+          console.log("open");
+          conn.send("hi!");
+        });
+        return conn;
       });
-      setConn(conn);
+
+      setMeAsPeer(peer);
+      setConns(conns);
     });
-  }, [me, target]);
+  }, [myId, peers]);
+
+  function sendToAll(message: string) {
+    conns.forEach((conn) => {
+      conn.send(message);
+    });
+  }
+  return { sendToAll };
+};
+
+const ConnectPage = () => {
+  const router = useRouter();
+  const { me, target } = router.query;
+  const [messages, setMessages] = useState<string[]>(["defa"]);
+  const [message, setMessage] = useState("");
+
+  const { sendToAll } = usePeerConn(me as string, target as string);
 
   function sendMessage(message: string) {
-    conn.send(message);
+    sendToAll(message);
   }
 
   return (
