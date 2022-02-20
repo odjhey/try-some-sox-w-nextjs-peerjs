48cc82107ac5ddde9fd45e4ea61169c3f65f62a4
odjhey <odjhey@gmail.com>
2022-02-20 17:07:22 +0800

    add sample me->target messaging

---
+++ pages/client/[me]/[target].tsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+const ConnectPage = () => {
+  const router = useRouter();
+  const { me, target } = router.query;
+  const [mePeer, setMePeer] = useState<any>(null);
+  const [conn, setConn] = useState<any>(null);
+  const [messages, setMessages] = useState<string[]>(["defa"]);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    if (!(me && target)) {
+      return;
+    }
+    import("peerjs").then((peerjs) => {
+      console.log("me", me);
+      console.log("target", target);
+      const peer = new peerjs.default(me as string, {
+        host: "localhost",
+        port: 9000,
+        path: "/myapp",
+      });
+
+      peer.on("connection", (conn) => {
+        conn.on("data", (data) => {
+          // Will print 'hi!'
+          setMessages((messages) => [...messages, data]);
+          console.log(data);
+        });
+      });
+      setMePeer(peer);
+
+      const conn = peer.connect(target as string);
+      conn.on("open", () => {
+        console.log("open");
+        conn.send("hi!");
+      });
+      setConn(conn);
+    });
+  }, [me, target]);
+
+  function sendMessage(message: string) {
+    conn.send(message);
+  }
+
+  return (
+    <div>
+      <p>details: {JSON.stringify(router.query)}</p>
+      <input type="text" onChange={(e) => setMessage(e.target.value)}></input>
+      <button onClick={() => sendMessage(message)}>send</button>
+
+      {messages.map((message, idx) => (
+        <p key={idx}>{message}</p>
+      ))}
+    </div>
+  );
+};
+
+export default ConnectPage;
